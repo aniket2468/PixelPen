0089028e7ab9a96ca7214428359fc28d04b0eee0
Aniket Sharma <aniketsharma2468@gmail.com>
2024-06-30 21:31:21 -0400

    woking on prisma and mongodb

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -10,10 +10,23 @@ import {
   } from "@fortawesome/free-solid-svg-icons";
 import ReactQuill from 'react-quill';
 import "react-quill/dist/quill.bubble.css";
+import { useRouter } from 'next/navigation';
+import { useSession } from 'next-auth/react';
 
 const WritePage = () => {
     const [open, setOpen] = useState(false)
     const [value, setValue] = useState("")
+
+    const { status } = useSession()
+    const router = useRouter()
+
+    if(status === "loading"){
+      return <div className={styles.loading}>Loading...</div>
+    }
+
+    if(status === "authenticated"){
+      router.push("/")
+    }
 
   return (
     <div className={styles.container}>
--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -3,20 +3,22 @@ import React from 'react'
 import styles from "./authLinks.module.css"
 import Link from 'next/link'
 import { useState } from 'react'
+import { signOut, useSession } from 'next-auth/react'
 
 const AuthLinks = () => {
 
     const [open, setOpen] = useState(false)
 
-    const status = "nonauthenticated"
+    const {status} = useSession();
+
     return (
     <>
-    {status==="authenticated" ? (
+    {status==="unauthenticated" ? (
         <Link href="/login" className={styles.link}>Login</Link>
         ) : (
         <>
         <Link href="/write">Write</Link>
-        <span className={styles.link}>Logout</span>
+        <span className={styles.link} onClick={signOut}>Logout</span>
         </>
         )}
         <div className={`${styles.burger} ${open ? styles.open : ''}`} onClick={()=>setOpen(!open)}>
