8d3124b244bcc340505f4ec04c3be337be355d02
Aniket Sharma <aniketsharma2468@gmail.com>
2024-07-13 13:32:25 -0400

    removed next auth and prisma

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,48 +1,45 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./authLinks.module.css"
 import Link from 'next/link'
-import { useState } from 'react'
 import { signOut, useSession } from 'next-auth/react'
 
 const AuthLinks = () => {
+  const [open, setOpen] = useState(false)
+  const { status } = useSession();
 
-    const [open, setOpen] = useState(false)
-
-    const {status} = useSession();
-
-    return (
+  return (
     <>
-    {status==="unauthenticated" ? (
+      {status === "unauthenticated" ? (
         <Link href="/login" className={styles.link}>Login</Link>
-        ) : (
+      ) : (
         <>
-        <Link href="/write">Write</Link>
-        <span className={styles.link} onClick={signOut}>Logout</span>
+          <Link href="/write">Write</Link>
+          <span className={styles.link} onClick={signOut}>Logout</span>
         </>
-        )}
-        <div className={`${styles.burger} ${open ? styles.open : ''}`} onClick={()=>setOpen(!open)}>
-            <div className={styles.line}></div>
-            <div className={styles.line}></div>
-            <div className={styles.line}></div>
+      )}
+      <div className={`${styles.burger} ${open ? styles.open : ''}`} onClick={() => setOpen(!open)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+      {open && (
+        <div className={styles.responsiveMenu}>
+          <Link href="/">Homepage</Link>
+          <Link href="/">Contact</Link>
+          <Link href="/">About</Link>
+          {status === "unauthenticated" ? (
+            <Link href="/login">Login</Link>
+          ) : (
+            <>
+              <Link href="/write">Write</Link>
+              <span className={styles.link}>Logout</span>
+            </>
+          )}
         </div>
-        {open && (
-            <div className={styles.responsiveMenu}>
-                <Link href="/">Homepage</Link>
-                <Link href="/">Contact</Link>
-                <Link href="/">About</Link>
-                {status==="nonauthenticated" ? (
-                <Link href="/login">Login</Link>
-                ) : (
-                <>
-                <Link href="/write">Write</Link>
-                <span className={styles.link}>Logout</span>
-                </>
-                )}
-            </div>
-        )}
+      )}
     </>
-    )
+  )
 }
 
 export default AuthLinks
