c91fac0674a44ffe59d443d94918cfdadceef13f
Aniket Sharma <aniketsharma2468@gmail.com>
2024-07-24 18:02:32 -0400

    authlink profile page fix

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from "./authLinks.module.css";
 import Link from 'next/link';
 import { signOut, useSession } from 'next-auth/react';
@@ -10,8 +10,20 @@ import { faUser } from '@fortawesome/free-solid-svg-icons';
 const AuthLinks = () => {
   const { data: session, status } = useSession();
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [username, setUsername] = useState(session?.user?.username);
 
-  const userProfilePic = session?.user?.image;
+  useEffect(() => {
+    const fetchUsername = async () => {
+      if (status === 'authenticated' && !username) {
+        const res = await fetch('/api/user');
+        if (res.ok) {
+          const userData = await res.json();
+          setUsername(userData.username);
+        }
+      }
+    };
+    fetchUsername();
+  }, [status, username]);
 
   const toggleDropdown = () => {
     setDropdownOpen(!dropdownOpen);
@@ -21,6 +33,8 @@ const AuthLinks = () => {
     signOut();
     setDropdownOpen(false);
   };
+
+  const userProfilePic = session?.user?.image;
 
   return (
     <div className={styles.authLinksContainer}>
@@ -40,7 +54,7 @@ const AuthLinks = () => {
           <Link href="/write" className={styles.link}>Write</Link>
           <div className={styles.profilePicContainer} onClick={toggleDropdown}>
             <Image
-              src={userProfilePic}
+              src={userProfilePic || <FontAwesomeIcon icon={faUser} className={styles.profileIcon} />}
               alt="Profile Picture"
               className={styles.profilePic}
               width={40}
@@ -49,7 +63,7 @@ const AuthLinks = () => {
           </div>
           {dropdownOpen && (
             <div className={styles.dropdownMenu}>
-              <Link href="/profile" className={styles.dropdownItem} onClick={() => setDropdownOpen(false)}>Profile Settings</Link>
+              <Link href={`/${username}`} className={styles.dropdownItem} onClick={() => setDropdownOpen(false)}>Profile Settings</Link>
               <span className={styles.dropdownItem} onClick={handleSignOut}>Logout</span>
             </div>
           )}
